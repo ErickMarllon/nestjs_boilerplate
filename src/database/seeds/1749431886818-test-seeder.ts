870ca67f76cf3e916a8aebd6b617c0deb3f1a6ac
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-06-16 16:25:34 -0300

    feat(auth): add controller endpoints structure
    add endpoint definitions and decorators for authentication controller

--- src/database/seeds/1749431886818-test-seeder.ts
+++ src/database/seeds/1749431886818-test-seeder.ts
@@ -4,15 +4,12 @@ import { SYSTEM_USER_ID } from '@/constants/app.constant';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import crypto from 'crypto';
 import { DataSource } from 'typeorm';
-import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { Seeder } from 'typeorm-extension';
 
 export class TestSeeder1749431886818 implements Seeder {
   track = false;
 
-  public async run(
-    dataSource: DataSource,
-    factoryManager: SeederFactoryManager,
-  ): Promise<any> {
+  public async run(dataSource: DataSource): Promise<any> {
     const sessionRepository = dataSource.getRepository(SessionEntity);
     const userRepository = dataSource.getRepository(UserEntity);
 
